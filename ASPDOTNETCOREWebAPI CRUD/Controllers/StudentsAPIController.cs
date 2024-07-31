17ecfced3e0ef87124766f76d665f7f64dc6b3b6
Rizwan682 <riz864990@gmail.com>
2024-07-31 17:29:05 +0500

    New .Net Core Web Api  Project using ADO

---
+++ ASPDOTNETCOREWebAPI CRUD/Controllers/StudentsAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDOTNETCOREWebAPI_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsAPIController : ControllerBase
+    {
+    }
+}
